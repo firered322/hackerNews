52aedf316586697d22752c55e0822bd65ef516ca
Atul <atulsingh1396@gmail.com>
2020-07-29 14:59:05 +0530

    rendering newsitems in the list page

--- src/components/newsitem/NewsItem.js
+++ src/components/newsitem/NewsItem.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "./NewsItem.css";
+import { findStory } from "../../api/index";
 
-const NewsItem = (props) => {
-  return (
-  <p>Story {props.story}</p>
-  );
-};
+class NewsItem extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      story: {},
+    };
+  }
+
+  componentDidMount() {
+    findStory(this.props.story).then(
+      (data) => data && this.setState({ story: data })
+    );
+  }
+
+  render() {
+    return this.state.story ? <p>{this.state.story.title}</p> : "loading";
+  }
+}
 
 export default NewsItem;
--- src/components/newslist/NewsList.js
+++ src/components/newslist/NewsList.js
@@ -1,8 +1,7 @@
 import React from "react";
 import "./NewsList.css";
 import NewsItem from "../newsitem/NewsItem";
-import { fetchTopStoryIds, findStory  } from "../../api/index";
-const axios = require("axios");
+import { fetchTopStoryIds } from "../../api/index";
 
 class NewsList extends React.Component {
   constructor() {
@@ -13,13 +12,13 @@ class NewsList extends React.Component {
   }
 
   componentDidMount() {
-    fetchTopStoryIds().then(data=>this.setState({storyIds: data}))
+    fetchTopStoryIds().then((data) => this.setState({ storyIds: data }));
   }
 
   render() {
     // const news = this.state.articles.map((item) => <p>{item}</p>);
     // return this.state.articles ? news : "loading";
-    return this.state.storyIds.map(item=><NewsItem story={item} />)
+    return this.state.storyIds.map((item) => <NewsItem story={item} />);
   }
 }
 
